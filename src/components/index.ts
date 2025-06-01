738a7a66e090ed71aeea0c5f67bc31b67ad1e2d5
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-01 19:01:36 -0400

    Fix TaskBar export path

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as TaskBar } from './TaskBar';
+export { default as TaskBar } from './TaskBar/';
 export { default as Modal } from './Modal';
 export { default as CreateScenarioModal } from './CreateScenarioModal';
