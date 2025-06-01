af320942ce61db956d20d81e16e8490aad9c255e
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-01 18:47:18 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { TaskBar } from './components/TaskBar';
+import { TaskBar } from './components';
 import { HomePage } from './pages';
 
 function App() {
