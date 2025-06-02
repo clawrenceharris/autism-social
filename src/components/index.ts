fba929b1b8836c90df94018ea0665de653ed9b33
Caleb Harris <clawrenceharris@gmail.com>
2025-06-02 11:25:10 -0400

    fixes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,1 @@
-export { default as TaskBar } from './TaskBar/';
-export { default as Modal } from './Modal';
-export { default as CreateScenarioModal } from './CreateScenarioModal';
+export { default as TaskBar } from "./TaskBar";
