ca1c9d930758f459bc03c495a3b251eb4dd0be38
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-02 20:36:30 -0400

    Updated SelectDialogueModal.tsx

---
+++ src/components/modals/SelectDialogueModal/SelectDialogueModal.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { SCENARIO_CATEGORIES } from "../../../constants";
+import { createScenario } from "../../../services/scenarios";
+import { useModal } from "../../../context";
+import {useDialogueScenarios} from "../../../hooks";
+import "./CreateScenarioModal.css";
+
+const SelectDialogueModal = () => {
+  const { closeModal } = useModal();
+  const { dialogues, loading, error } = useDialogueScenarios();
+
+  return (
+    <div>
+      {dialogues.map((item) =>(
+      <button className="card">
+      <div>
+        <h2>{item.title}</h2>
+        <p><small>ID: {item.id}</small></p>
+      </div>
+        
+      </button>
+      ))}
+    </div>
+  );
+};
+
+export default SelectDialogueModal;
