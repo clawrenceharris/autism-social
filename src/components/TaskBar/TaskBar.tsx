bc90b93a93096aedb9a3602fc06006d97a144e38
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-03 12:05:41 -0400

    Update Navigation Bar Styles

--- src/components/TaskBar/TaskBar.tsx
+++ src/components/TaskBar/TaskBar.tsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
-import { Home, FolderOpen, Settings } from "lucide-react";
+import { Home, FolderOpen, Settings, Zap } from "lucide-react";
 import "./TaskBar.css";
 
 const TaskBar = () => {
   return (
     <nav className="taskbar">
       <div className="taskbar-container">
+        <Link to="/" className="taskbar-brand">
+          <Zap size={24} />
+          <span>Scenario Builder</span>
+        </Link>
+        
         <div className="taskbar-nav">
           <Link to="/" className="taskbar-link">
-            <Home size={20} />
+            <Home />
             <span>Home</span>
           </Link>
           <Link to="/scenarios" className="taskbar-link">
-            <FolderOpen size={20} />
+            <FolderOpen />
             <span>Scenarios</span>
           </Link>
           <Link to="/settings" className="taskbar-link">
-            <Settings size={20} />
+            <Settings />
             <span>Settings</span>
           </Link>
         </div>
