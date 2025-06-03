6d936f5af62fa29da166a98d828c38e52cafee33
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-02 20:21:49 -0400

    Implement Modal Context and Refactor Modal Usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RootLayout, ScenarioLayout, TaskBar } from "./components";
 import { HomePage, ScenarioPage, ScenariosPage } from "./pages";
+import { ModalProvider } from "./context";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <TaskBar />
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/scenarios" element={<ScenariosPage />} />
-          </Route>
-          <Route element={<ScenarioLayout />}>
-            <Route path="/scenario/:scenarioId" element={<ScenarioPage />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    <ModalProvider>
+      <Router>
+        <div className="container">
+          <TaskBar />
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/scenarios" element={<ScenariosPage />} />
+            </Route>
+            <Route element={<ScenarioLayout />}>
+              <Route path="/scenario/:scenarioId" element={<ScenarioPage />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </ModalProvider>
   );
 }
 
