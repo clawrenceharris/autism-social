442ff800f6a0633f13d53f8e392dbe1dbe1a30af
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-05 18:47:50 -0400

    Updated LoginPage.tsx

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -44,7 +44,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className=" container flex items-center justify-center p-4">
+    <div className=" container flex items-center ">
       <div className="w-full max-w-md">
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold mb-2">Log in</h1>
