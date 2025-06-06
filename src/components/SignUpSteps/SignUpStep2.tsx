e901f4e6f3cfc11d362ecdc43f1458daee774461
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-06 06:30:47 -0400

    Updated SignUpStep2.tsx

--- src/components/SignUpSteps/SignUpStep2.tsx
+++ src/components/SignUpSteps/SignUpStep2.tsx
@@ -13,7 +13,7 @@ export const SignUpStep2 = ({
 }: SignUpStep2Props) => {
   const { goals, loading, error } = useGoals();
 
-  if (true)
+  if (loading)
     return (
       <div className="center-absolute">
         <ProgressIndicator />
