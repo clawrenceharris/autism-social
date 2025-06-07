a9131756da5776f4d92772eb7ff101bbb13dd2ba
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-07 15:21:09 -0400

    Updated progress.ts

--- src/services/progress.ts
+++ src/services/progress.ts
@@ -11,3 +11,24 @@ export async function getProgress(userId: string): Promise<UserProgress> {
 
   return data;
 }
+
+export async function createProgress(userId: string): Promise<UserProgress> {
+  const { data, error } = await DatabaseService.create<UserProgress>(
+    "user_progress",
+    {
+      id: "",
+      user_id: userId;
+      clarity: 0;
+      empathy: 0;
+      assertiveness: 0;
+      social_awareness: 0;
+      self_advocacy: 0;
+          
+      
+    }
+  );
+
+  if (error) throw error;
+
+  return data;
+}
