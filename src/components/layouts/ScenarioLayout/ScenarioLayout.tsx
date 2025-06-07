b6b2ebf2a00ec98b7ff71f0abe9552a9babe32d7
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-06 21:05:26 -0400

    Updated ScenarioLayout.tsx

--- src/components/layouts/ScenarioLayout/ScenarioLayout.tsx
+++ src/components/layouts/ScenarioLayout/ScenarioLayout.tsx
@@ -5,7 +5,7 @@ import { ScenarioProvider } from "../../../context";
 const ScenarioLayout = () => {
   const { scenarioId } = useParams<{ scenarioId: string }>();
   if (!scenarioId) {
-    return <div>No Scenario could be found</div>;
+    return <div className="center-absolute">No Scenario could be found</div>;
   }
   return (
     <ScenarioProvider scenarioId={scenarioId}>
