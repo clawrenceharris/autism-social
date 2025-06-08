3ec1733704874d96355543ceb5166b39f3921928
Caleb Harris <clawrenceharris@gmail.com>
2025-06-08 15:12:32 -0400

    Fixed Imports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
-export { default as useScenarios } from "./useScenarios";
 export { default as useScenarioDialogues } from "./useScenarioDialogues";
-export { default as useDialogues } from "./useDialogues";
 
 // Re-export React Query hooks
 export * from "./queries/useScenarios";
