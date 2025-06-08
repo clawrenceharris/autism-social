6e025dfe6622385731fdbc6ed793791ecdfb60ab
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-08 15:27:14 -0400

    Updated UserLayout.tsx

--- src/components/layouts/UserLayout/UserLayout.tsx
+++ src/components/layouts/UserLayout/UserLayout.tsx
@@ -5,7 +5,7 @@ import type { AuthContextType } from "../../../types";
 const UserLayout = () => {
   const context = useOutletContext<AuthContextType>();
   return (
-    <div>
+    <div className="layout-container">
       <UserHeader />
       <main>
         <Outlet context={context} />
