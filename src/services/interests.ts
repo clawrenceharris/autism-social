d5d05f233d8165c4ffe7ab22cf81302d14c474da
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-09 14:21:00 -0400

    Updated interests.ts

--- src/services/interests.ts
+++ src/services/interests.ts
@@ -79,7 +79,7 @@ export async function updateUserInterests(
   userId: string,
   new_interests: string[]
 ): Promise<void> {
-  const { data, error } = await supabase.rpc("update_user_interests_by_name", {
+  const { data, error } = await supabase.rpc("update_user_interests", {
     user_uuid: userId,
     new_interests,
   });
