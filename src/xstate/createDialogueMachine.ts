08b8b16bf7c374516530189a676dbd65638dcb11
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-10 14:52:52 -0400

    Updated createDialogueMachine.ts

--- src/xstate/createDialogueMachine.ts
+++ src/xstate/createDialogueMachine.ts
@@ -28,6 +28,7 @@ export function createDialogueMachine(
               const updated = { ...context };
               opt.scores.forEach((key) => {
                 updated[key] += 1;
+                console.log("Value: " + updated[key]);
               });
             console.log(updated);
               return updated;
