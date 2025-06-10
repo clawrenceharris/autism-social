5f139e49d8a0a3870121b4da1befeb22e7114f9c
caleb <84045966+clawrenceharris@users.noreply.github.com>
2025-06-10 16:18:38 -0400

    Updated DashboardPage.tsx

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -65,7 +65,7 @@ const DashboardPage = () => {
   };
 
   return (
-    <div className="container">
+    <div >
       <div className="dashboard-header">
         <div className="welcome-section">
           <h1 className="welcome-message">
