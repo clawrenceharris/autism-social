a19f988814ca6a0453bc4d14c40e73e3e26da702
Caleb Harris <clawrenceharris@gmail.com>
2025-06-11 14:49:37 -0400

    Removed tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./styles/global.scss";
 import App from "./App.tsx";
-import { queryClient } from "./lib/queryClient";
 import {
   ModalProvider,
   UserProvider,
@@ -15,18 +12,15 @@ import { ToastContainer } from "./components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <ToastProvider>
-          <UserProvider>
-            <ModalProvider>
-              <App />
-              <ToastContainer />
-            </ModalProvider>
-          </UserProvider>
-        </ToastProvider>
-      </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <ToastProvider>
+        <UserProvider>
+          <ModalProvider>
+            <App />
+            <ToastContainer />
+          </ModalProvider>
+        </UserProvider>
+      </ToastProvider>
+    </AuthProvider>
   </StrictMode>
 );
